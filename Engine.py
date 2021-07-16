7daa16b740da342724c44718ba293f21cb417bf1
Skultz <33635788+DNSKT@users.noreply.github.com>
2021-07-15 21:37:34 -0500

    added loggin
    yyes

--- Engine.py
+++ Engine.py
@@ -5,8 +5,15 @@ import discord
 import random
 from discord import message
 from discord.ext import commands
+import logging
 
-token = ' '
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
+token = 'ODYyMTMxMzMxNTgwMDM1MTA0.YOT4Xw.73Hw8-kN34bQpx8wOPXpnUdmH98'
 
 
 description = '''yes'''
