c71ed4fac301388759a33d5f8120f947052559a8
Skultz <33635788+DNSKT@users.noreply.github.com>
2021-07-22 23:40:17 -0500

    AI of the discord bot

---
+++ NetworkWatcherRG.py
@@ -0,0 +1,52 @@
+from azure.cognitiveservices.vision.computervision import ComputerVisionClient
+from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
+from azure.cognitiveservices.vision.computervision.models import VisualFeatureTypes
+from msrest.authentication import CognitiveServicesCredentials
+
+from array import array
+import os
+from PIL import Image
+import sys
+import time
+
+'''
+Authenticate
+Authenticates your credentials and creates a client.
+'''
+subscription_key = "95230f00389c4bab9d4353479ebdebda"
+endpoint = "https://southcentralus.api.cognitive.microsoft.com/"
+
+
+computervision_client = ComputerVisionClient(endpoint, CognitiveServicesCredentials(subscription_key))
+
+'''
+OCR: Read File using the Read API, extract text - remote
+This example will extract text in an image, then print results, line by line.
+This API call can also extract handwriting style text (not shown).
+'''
+print("===== Read File - remote =====")
+# Get an image with text
+read_image_url = "https://cdn.discordapp.com/attachments/797284011059576872/867230021428314162/levelup.png"
+
+# Call API with URL and raw response (allows you to get the operation location)
+read_response = computervision_client.read(read_image_url,  raw=True)
+
+# Get the operation location (URL with an ID at the end) from the response
+read_operation_location = read_response.headers["Operation-Location"]
+# Grab the ID from the URL
+operation_id = read_operation_location.split("/")[-1]
+
+# Call the "GET" API and wait for it to retrieve the results 
+while True:
+    read_result = computervision_client.get_read_result(operation_id)
+    if read_result.status not in ['notStarted', 'running']:
+        break
+    time.sleep(1)
+
+# Print the detected text, line by line
+if read_result.status == OperationStatusCodes.succeeded:
+    for text_result in read_result.analyze_result.read_results:
+        for line in text_result.lines:
+            print(line.text)
+            print(line.bounding_box)
+print()
