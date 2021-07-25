5f5552c736b6c812e657350892032d50a6f1e79c
Skultz <33635788+DNSKT@users.noreply.github.com>
2021-07-24 23:59:55 -0500

    some source codes, nothing really important at all

---
+++ translate.py/NetworkTranslateDetect.py
@@ -0,0 +1,31 @@
+import requests, uuid, json
+
+subscription_key = ""
+endpoint = ""
+
+location = ""
+
+path = '/translate'
+constructed_url = endpoint + path
+
+params = {
+    'api-version': '3.0',
+    'to': ['de', 'it']
+}
+constructed_url = endpoint + path
+
+headers = {
+    'Ocp-Apim-Subscription-Key': subscription_key,
+    'Ocp-Apim-Subscription-Region': location,
+    'Content-type': 'application/json',
+    'X-ClientTraceId': str(uuid.uuid4())
+}
+
+body = [{
+    'text': 'Hello World!'
+}]
+
+request = requests.post(constructed_url, params=params, headers=headers, json=body)
+response = request.json()
+
+print(json.dumps(response, sort_keys=True, ensure_ascii=False, indent=4, separators=(',', ': ')))
